e178037ceeadff881769be1ca43a0c7d0cb2d476
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-01-20 18:30:45 -0300

    error handler in console, and client view

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import "express-async-errors"; //handle async erros in API.
 import "./database/connections";
 import routes from "./routes/routes";
+import errorHandler from "./errors/handler";
 
 const app = express();
 app.use(express.json());
 
 app.use(routes);
+app.use(errorHandler);
 
 app.listen(3333, () => {
   console.log("SERVER ON");
