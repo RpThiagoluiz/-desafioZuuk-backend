b9927e64826a1c7ea313f1161a50db3b5057487e
RpThiagoluiz <rp.thiagoluiz@gmail.com>
2021-01-20 17:05:18 -0300

    create customers in database on

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import { getRepository } from "typeorm";
-import Customers from "./model/Customer";
+import Customers from "./models/Customer";
 import "./database/connections";
 
 const app = express();
 app.use(express.json());
 
-app.post("/clientes/cadastro", (request, response) => {
+app.post("/clientes/cadastro", async (request, response) => {
   console.log(request.body);
 
   const {
@@ -21,6 +21,19 @@ app.post("/clientes/cadastro", (request, response) => {
   } = request.body;
 
   const customersRepository = getRepository(Customers);
+
+  const customer = customersRepository.create({
+    name,
+    cep,
+    address,
+    district,
+    city,
+    state,
+    phone,
+    email,
+  });
+
+  await customersRepository.save(customer);
 
   return response.json({ message: "Hello, world!" });
 });
